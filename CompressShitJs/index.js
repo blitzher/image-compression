fe28f43c07e80b6b76fbfe6f8c15be70a3074e6f
klem-bot-delta <54005826+klem-bot-delta@users.noreply.github.com>
2021-02-03 10:01:41 +0100

    A little taste of the compressssss

---
+++ CompressShitJs/index.js
@@ -0,0 +1,40 @@
+let { readFileSync } = require("fs");
+
+
+const table = ( arr ) =>
+      Array.from( new Set( arr ) )
+      .map( x => arr.map( y => ( y == x ) ? 1 : 0 ) );
+
+
+const encode = arr =>
+      [ arr.length + 1, ...table( arr )
+        .flatMap( ( x, i ) =>
+                  [ Array.from( new Set(arr) )[i], x ] ).flat(2) ];
+
+
+const decode = arr => {
+    let str = arr.slice( 1 );
+
+    let [ newArr, out ] = [ [], [].fill( 0, 0, arr[1] ) ];
+
+    for ( i = 0; i < str.length; i += arr[0] ) {
+        newArr.push( str.slice( i, i + arr[0] ) );
+    }
+
+
+    newArr.map( xs => [ xs[0], xs.slice( 1 ) ] )
+        .forEach( x =>
+                  x[1].forEach( ( y, i ) =>
+                                { if ( y == 1 ) { out[i] = x[0]; } } ) );
+
+    return out.join("");
+}
+
+
+let src = Array.from( "Hello,World! Hello,World! Hello,World!");
+let encoded = encode( src );
+let decoded = decode( encoded );
+
+console.log( src, "\n" );
+console.log( encoded, "\n" );
+console.log( decoded);
