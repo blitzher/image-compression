483c9e860a156743a178412d9d43cd9fdbc81b17
klem-bot-delta <54005826+klem-bot-delta@users.noreply.github.com>
2021-04-07 14:50:55 +0200

    Adopted BEM and changes to comments.

--- product/mockup/index.js
+++ product/mockup/index.js
@@ -1,76 +1,75 @@
-const WebSocket = require("ws");
-const express = require("express");
-const fileUpload = require("express-fileupload");
+const express = require('express');
+const fileUpload = require('express-fileupload');
 const app = express();
-const expressWs = require("express-ws")(app);
-const fs = require("fs");
-const path = require("path");
+const expressWs = require('express-ws')(app);
+const fs = require('fs');
 
 const port = process.argv[2];
 
-if (!fs.existsSync("uploads") || !fs.statSync("uploads").isDirectory()) {
-  fs.mkdirSync("uploads");
+if (!fs.existsSync('uploads') || !fs.statSync('uploads').isDirectory()) {
+    fs.mkdirSync('uploads');
 }
 
 app.use(fileUpload());
 app.use(express.json());
-app.use("/", express.static(__dirname + "/public"));
+app.use('/', express.static(__dirname + '/public')); // Serves client-side files.
+app.use('/plugin', express.static(__dirname + '/../plugin/static')); // Serves the plugin on a seperate route.
+app.use('/api/images', express.static(__dirname + '/uploads')); // Serves uploaded images.
+let wss = expressWs.getWss('/');
 
-app.use("/plugin", express.static(__dirname + "/../plugin/static"));
-app.use("/api/images", express.static(__dirname + "/uploads"));
-let wss = expressWs.getWss("/");
-
+// Semi persistent message history for new connections
 const messageHistory = [
-  {
-    displayName: "Cool Server😎",
-    timestamp: new Date().getTime(),
-    body: "🍔🍔",
-  },
+    {
+        displayName: 'Cool Server😎',
+        timestamp: new Date().getTime(),
+        body: '🍔🍔',
+    },
 ];
 
-app.ws("/", (ws, req) => {
-  //ws.send('{"displayName": "", "body": "Hello, Client!"}');
+// Primary communications socket.
+app.ws('/', (ws, req) => {
+    ws.on('message', (message) => {
+        console.log(message);
 
-  ws.on("message", (message) => {
-    console.log(message);
+        const msg = JSON.parse(message);
+        wss.clients.forEach((client) => {
+            if (msg.body === ';clear') {
+                client.send(';clear');
+                messageHistory.splice(1, messageHistory.length);
+                client.send(JSON.stringify(messageHistory[0]));
+            } else {
+                client.send(message);
+            }
+        });
 
-    const msg = JSON.parse(message);
-    wss.clients.forEach((client) => {
-      if (msg.body === ";clear") {
-        client.send(";clear");
-        messageHistory.splice(1, messageHistory.length);
-        client.send(JSON.stringify(messageHistory[0]));
-      } else {
-        client.send(message);
-      }
+        if (msg !== ';clear') messageHistory.push(JSON.parse(message));
     });
-
-    if (msg !== ";clear") messageHistory.push(JSON.parse(message));
-  });
 });
 
-app.get("/api/get_message_history/", (req, res) => {
-  res.send(messageHistory);
+// Route: Message message history.
+app.get('/api/get_message_history/', (req, res) => {
+    res.send(messageHistory);
 });
 
-app.post("/api/upload", (req, res) => {
-  let file;
-  let uploadPath;
+// Route: Image uploads.
+app.post('/api/upload', (req, res) => {
+    let file;
+    let uploadPath;
 
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send("No files were uploaded.");
-  }
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
 
-  file = req.files.image;
-  uploadPath = __dirname + "/uploads/" + file.name;
+    file = req.files.image;
+    uploadPath = __dirname + '/uploads/' + file.name;
 
-  file.mv(uploadPath, (err) => {
-    if (err) return res.status(500).send(err);
+    file.mv(uploadPath, (err) => {
+        if (err) return res.status(500).send(err);
 
-    res.send(`/uploads/${file.name}`);
-  });
+        res.send(`/uploads/${file.name}`);
+    });
 });
 
 app.listen(port, () => {
-  console.log(`server listening on ${port}`);
+    console.log(`server listening on ${port}`);
 });
--- product/mockup/public/js/init.js
+++ product/mockup/public/js/init.js
@@ -1,160 +1,199 @@
 /* init.js */
 
+/* Helpers */
 export const $ = (id) => document.getElementById(id);
 export const _f = document.forms;
 
-/* private */
+/**
+ * Private bubble element to display messages in chat.
+ * @param {String} text
+ * @param {String} time
+ * @param {String} user
+ * @returns
+ */
 const bubbleElem = (text, time, user) => {
-  const bubble = document.createElement("div");
-  bubble.className = "msg-bubble";
+    const bubble = document.createElement('div');
+    bubble.className = 'bubble';
 
-  bubble.innerHTML = `
-	<p class="bubble-user">${user ? user + " says:" : ""}</p>
-	<p class="bubble-body">${text}</p>
-	<p class="bubble-time">${time}</p>`;
+    bubble.innerHTML = `
+	<p class="bubble__user">${user ? user + ' says:' : ''}</p>
+	<p class="bubble__body">${text}</p>
+	<p class="bubble__time">${time}</p>`;
 
-  return bubble;
+    return bubble;
 };
 
+/**
+ * Format and send message to server.
+ * @param {Object} session
+ * @param {String} contentType
+ * @param {String} imageSrc
+ * @returns
+ */
 export const sendMsg = (session, contentType, imageSrc) => {
-  let msg = msgForm.elements.msgInput.value.trim();
+    let msg = msgForm.elements.msgInput.value.trim();
 
-  if (msg === "" && !imageSrc) return;
+    if (msg === '' && !imageSrc) return;
 
-  const sendTime = new Date().getTime();
+    //Get time in milliseconds
+    const sendTime = new Date().getTime();
 
-  let payload =
-    contentType === "image"
-      ? {
-          displayName: session.displayName,
-          body: msgForm.elements.msgInput.value,
-          contentType: contentType,
-          imageSrc: imageSrc,
-          timestamp: sendTime,
-        }
-      : {
-          displayName: session.displayName,
-          body: msgForm.elements.msgInput.value,
-          contentType: contentType,
-          timestamp: sendTime,
-        };
+    let payload =
+        contentType === 'image'
+            ? {
+                  displayName: session.displayName,
+                  body: msgForm.elements.msgInput.value,
+                  contentType: contentType,
+                  imageSrc: imageSrc,
+                  timestamp: sendTime,
+              }
+            : {
+                  displayName: session.displayName,
+                  body: msgForm.elements.msgInput.value,
+                  contentType: contentType,
+                  timestamp: sendTime,
+              };
 
-  session.socket.send(JSON.stringify(payload));
+    session.socket.send(JSON.stringify(payload));
 
-  msgForm.elements.msgInput.value = "";
+    msgForm.elements.msgInput.value = '';
 };
 
-/* append a message to chatbox, as if it was received */
-export const receivedMsg = (msg, tone) => {
-  let chatbox = document.getElementById("chatbox");
+/**
+ * Append a message to chatbox, as if it was received
+ * @param {Object} msg
+ * @param {Boolean} toneEnabled
+ */
+export const receivedMsg = (msg, toneEnabled) => {
+    let chatbox = document.getElementById('chatbox');
 
-  const timestamp = new Date(msg.timestamp);
+    // Time object from timestamp (milliseconds)
+    const timestamp = new Date(msg.timestamp);
 
-  /**
-   * format such as 21:05
-   * or 9:05 PM
-   */
+    /**
+     * Format time such as 21:05
+     * or 9:05 PM
+     */
 
-  /* FOR FUTURE CONFIGS / SETTINGS */
-  const american_time = true;
+    /* FOR FUTURE CONFIGS / SETTINGS */
+    const american_time = true;
 
-  let formattedTime;
-  if (american_time) {
-    let hours = timestamp.getHours();
-    let minutes = timestamp.getMinutes().toString().padStart(2, "0");
-    const ampm = hours >= 12 ? "PM" : "AM";
-    hours = hours % 12 || 12;
-    formattedTime = hours + ":" + minutes + " " + ampm;
-  } else {
-    const hours = timestamp.getHours().toString();
-    const minutes = timestamp.getMinutes().toString().padStart(2, "0");
-    formattedTime = `${hours}:${minutes}`;
-  }
+    let formattedTime;
+    if (american_time) {
+        let hours = timestamp.getHours();
+        let minutes = timestamp.getMinutes().toString().padStart(2, '0');
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12 || 12;
+        formattedTime = hours + ':' + minutes + ' ' + ampm;
+    } else {
+        const hours = timestamp.getHours().toString();
+        const minutes = timestamp.getMinutes().toString().padStart(2, '0');
+        formattedTime = `${hours}:${minutes}`;
+    }
 
-  let bubble = bubbleElem(msg.body, formattedTime, msg.displayName);
+    let bubble = bubbleElem(msg.body, formattedTime, msg.displayName);
 
-  if (msg.contentType === "image") {
-    let image = document.createElement("img");
-    image.src = msg.imageSrc;
-    bubble.appendChild(image);
-  }
+    if (msg.contentType === 'image') {
+        let image = document.createElement('img');
+        image.src = msg.imageSrc;
+        bubble.appendChild(image);
+    }
 
-  if (msg.displayName === sessionStorage.getItem("displayName")) {
-    bubble.classList.add("msg-bubble-my");
-  }
+    if (msg.displayName === sessionStorage.getItem('displayName')) {
+        bubble.classList.add('bubble--my');
+    }
 
-  if (tone && msg.displayName !== sessionStorage.getItem("displayName")) {
-    const audio = new Audio(`tone.mp3`);
-    audio.volume = $("volume").value / 100;
-    audio.play();
-  }
+    if (
+        toneEnabled &&
+        msg.displayName !== sessionStorage.getItem('displayName')
+    ) {
+        const audio = new Audio(`tone.mp3`);
+        audio.volume = $('volume').value / 100;
+        audio.play();
+    }
 
-  chatbox.appendChild(bubble);
-  chatbox.parentElement.scrollTo(0, chatbox.parentElement.scrollHeight);
+    chatbox.appendChild(bubble);
+    chatbox.parentElement.scrollTo(0, chatbox.parentElement.scrollHeight);
 };
 
+/**
+ * Application entrypoint.
+ * @param {String} host Host name
+ * @returns
+ */
 export const init = (host) => {
-  let clientSession = {
-    displayName: sessionStorage.getItem("displayName"),
-    socket: new WebSocket(`ws://${host}`),
-  };
-
-  /* get message history from server */
-  fetch(window.location + "api/get_message_history")
-    .then((response) => response.json())
-    .then((data) => {
-      clientSession["msgHistory"] = data;
-      data.forEach((msg) => {
-        receivedMsg(msg, false);
-      });
-    });
-
-  $("currentUser").innerText = "User: " + clientSession.displayName;
-
-  return clientSession;
-};
-
-function fileValidation(file) {    
-  // Allowing file type
-  var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.webp)$/i;
-  
-  if (!allowedExtensions.exec(file)) {
-    alert('Invalid file type');
-    return false;
-  }else{
-    return true;
-  }
-}
-
-export const sendImage = (host, session) => {
-  let xhr = new XMLHttpRequest();
-  let file = $("imageUpload").files[0];
-
-  const fileSize = file.size / Math.pow(2, 20);
-  if (fileSize > 5) {
-    $("imageUpload").value = null; //does not work
-    alert("Filesize exceeds file limit");
-    return;
-  }
-
-  let formData = new FormData();
-  formData.append("image", file);
-
-  if (typeof file !== "undefined" && fileValidation(file.name) == true) {
-    xhr.open("POST", `/api/upload`, true);
-    xhr.onload = () => {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log("success");
-      }
+    let clientSession = {
+        displayName: sessionStorage.getItem('displayName'),
+        socket: new WebSocket(`ws://${host}`),
     };
 
-    $("imageUpload").value = null;
-    const previews = document.getElementsByClassName("image-preview");
-    for (let i = previews.length - 1; i >= 0; i--) {
-      previews[i].remove();
-    }
-    xhr.send(formData);
+    /* Get message history from server */
+    fetch(window.location + 'api/get_message_history')
+        .then((response) => response.json())
+        .then((data) => {
+            clientSession['msgHistory'] = data;
+            data.forEach((msg) => {
+                receivedMsg(msg, false);
+            });
+        });
 
-    sendMsg(session, "image", `http://${host}/api/images/${file.name}`);
-  }
+    $('currentUser').innerText = 'User: ' + clientSession.displayName;
+
+    return clientSession;
+};
+
+/**
+ * Verify extension of uploaded image file.
+ * @param {String} filename
+ * @returns
+ */
+function fileValidation(filename) {
+    // Allowing file type
+    var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.webp)$/i;
+
+    if (!allowedExtensions.exec(filename)) {
+        alert('Invalid file type');
+        return false;
+    } else {
+        return true;
+    }
+}
+
+/**
+ * Image upload handler.
+ * @param {String} host
+ * @param {Object} session
+ * @returns
+ */
+export const sendImage = (host, session) => {
+    let xhr = new XMLHttpRequest();
+    let file = $('imageUpload').files[0];
+
+    const fileSize = file.size / Math.pow(2, 20);
+    if (fileSize > 5) {
+        $('imageUpload').value = null; //does not work
+        alert('Filesize exceeds file limit');
+        return;
+    }
+
+    let formData = new FormData();
+    formData.append('image', file);
+
+    if (typeof file !== 'undefined' && fileValidation(file.name) == true) {
+        xhr.open('POST', `/api/upload`, true);
+        xhr.onload = () => {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                console.log('success');
+            }
+        };
+
+        $('imageUpload').value = null;
+        const previews = document.getElementsByClassName('image-preview');
+        for (let i = previews.length - 1; i >= 0; i--) {
+            previews[i].remove();
+        }
+        xhr.send(formData);
+
+        sendMsg(session, 'image', `http://${host}/api/images/${file.name}`);
+    }
 };
